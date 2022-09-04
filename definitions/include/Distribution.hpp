11cf0a4b908c206d231221e9915dd8ac9a439304
syler <tureanandreibogdan@gmail.com>
2022-09-04 11:58:32 +0300

    Added doxygen support

--- definitions/include/Distribution.hpp
+++ definitions/include/Distribution.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include <MiniObjectDefs.hpp>
-
+/*!
+*   @brief Data distribution interface. Let distribute a package to all linked the receivers.
+*/
 struct DataDistribution
 {
+    /*!
+    *   @brief Distribute data to all the available receivers from the receiver pool.
+    *   @param package Const data package that will be delived to all the receivers.
+    *   @return true if the distribution succedded, false if any of the receiver fails to interpret the package.
+    *   @note Data distribution will be performed sequencially.
+    */
     virtual bool distributeData(DataPackageCPtr package) = 0;
 };
 
